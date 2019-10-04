c3b68ab794f70b73f21db34b21e35fe18a5be8a0
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2019-10-03 23:53:23 -0400

    Refactored to use callback function and spread parameter

--- src/index.js
+++ src/index.js
@@ -1,124 +1,47 @@
 import React, { useState } from "react"
-import { createStore } from "redux"
 import ReactDOM from "react-dom"
-
-import { Provider, connect, useSelector, useDispatch } from "react-redux"
 
 import "./styles.css"
 
-const axios = require("axios")
-
-function rootReducer(state = [], action) {
-  switch (action.type) {
-    case "ADD_SEARCH": {
-      const { query } = action.payload
-      return state.concat(query)
-    }
-    default:
-      return state
-  }
-}
-
 function App() {
   // React Hooks declarations
-  const [results, setResults] = useState([])
-  const [errors, setErrors] = useState("")
   const [searches, setSearches] = useState([])
   const [query, setQuery] = useState("")
 
-  // React-Redux section
-  const reduxSearches = useSelector(state => state)
-  const dispatch = useDispatch(query => query)
-
-  /**
-   * A hit returned by HackerNews API:
-   * @typedef {Component} Hit
-   * @property {string} created_at Timestamp in format "2017-05-01T01:55:04.000Z"
-   * @property {string} title
-   * @property {string} url
-   * @property {string} author
-   * @property {integer} points
-   */
-  const Hit = ({ created_at, title, url, author, points }) => (
-    <li>
-      <a href="{url}">{title}</a> by {author} ({points} points) on{" "}
-      {// Make timestamp readable by converting
-      // "2017-05-01T01:55:04.000Z" to
-      // "2017-05-01 at 01:55:04"
-      created_at.replace("T", " at ").replace(".000Z", "")}
-    </li>
-  )
-
-  const Search = ({ query }) => <li>{query}</li>
-
-  const searchHackerNews = function accessAPI(query) {
-    const encodedURI = window.encodeURI(
-      // relevance-ordered
-      `https://hn.algolia.com/api/v1/search?query=${query}`
-      // or: date-ordered
-      // `https://hn.algolia.com/api/v1/search_by_date?query=${query}`
-    )
-
-    function handleError(error) {
-      console.warn(error)
-      setErrors(error.toString())
-      return null
-    }
-
-    const updateResults = hits => {
-      // Update results state:
-      setResults(hits)
-    }
-
-    const makeCallToAPI = axios
-      .get(encodedURI)
-      .then(({ data }) => {
-        updateResults(data.hits)
-      })
-      .catch(handleError)
-  }
-
   const handleClick = () => {
-    searchHackerNews(query)
+    // Add the search term to the list onClick of Search button
+    // (Actually searching would require an API call here)
 
     // Save search term state to React Hooks
-    setSearches(searches.concat(query))
-
-    // Save search term state using Redux
-    dispatch({ type: "ADD_SEARCH", payload: { query } })
+    setSearches(searches => [...searches, query]) // setSearches(searches.concat(query))
+    // setSearches(searches.concat(query))
   }
 
-  const updateQuery = event => {
-    setQuery(event.target.value)
+  const updateQuery = ({ target }) => {
+    // Update query onKeyPress of input box
+    setQuery(target.value)
   }
 
-  const keyPressed = event => {
-    if (event.key === "Enter") {
+  const keyPressed = ({ key }) => {
+    // Capture search on Enter key
+    if (key === "Enter") {
       handleClick()
     }
   }
 
   const submitHandler = e => {
+    // Prevent form submission on Enter key
     e.preventDefault()
   }
 
+  const Search = ({ query }) => <li>{query}</li>
+
   return (
     <div className="App">
-      <h1>HackerNews API React / Redux Example</h1>
-      <h3>{errors}</h3>
+      <h1>Add an item to an array in React State</h1>
       <h2>Previous searches: (React Hooks)</h2>
-      <ul>
+      <ul className="previousSearch">
         {searches.map((query, i) => (
-          <Search
-            query={query}
-            // Prevent duplicate keys by appending index:
-            key={query + i}
-          />
-        ))}
-      </ul>
-      <h2>Previous searches: (Redux)</h2>
-      <ul>
-        {reduxSearches.map((query, i) => (
           <Search
             query={query}
             // Prevent duplicate keys by appending index:
@@ -147,37 +70,12 @@ function App() {
           </button>
         </div>
       </form>
-
-      <div className="break" />
-
-      <ul className="searchResults">
-        {results.map((hit, i) => (
-          <Hit
-            // HackerNews search hit returns 5 elements
-            created_at={hit.created_at}
-            title={hit.title}
-            url={hit.url}
-            author={hit.author}
-            points={hit.points}
-            // Prevent duplicate keys by appending index:
-            key={hit.title + i}
-          />
-        ))}
-      </ul>
     </div>
   )
 }
 
-export default connect()(App)
+export default App
 
 const rootElement = document.getElementById("root")
 
-// Create store for Redux
-const store = createStore(rootReducer)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-)
+ReactDOM.render(<App />, rootElement)
